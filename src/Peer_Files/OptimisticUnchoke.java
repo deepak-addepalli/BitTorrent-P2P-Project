0b5057922ff53e59cfd9b3470f112d094b5fa319
manishakatkam <mkatkam@ufl.edu>
2017-12-01 08:42:10 -0500

    Add files via upload

--- src/Peer_Files/OptimisticUnchoke.java
+++ src/Peer_Files/OptimisticUnchoke.java
@@ -3,16 +3,12 @@ import Logging.logrecord;
 
 import java.util.*;
 
-/**
- * Created by Harshit Vijayvargia on 4/3/2017.
- */
 public class OptimisticUnchoke implements Runnable {
 
     LinkedList<PeerInfo> unchokable = new LinkedList<PeerInfo>();
     HashSet<Integer> unchokablePeerIds = new HashSet<Integer>();
     PeerSetup _peerSetupObj;
 
-    //Constructor
     public OptimisticUnchoke(PeerSetup obj)
     {
         _peerSetupObj = obj;
@@ -20,7 +16,7 @@ public class OptimisticUnchoke implements Runnable {
 
     synchronized void setUnchokable(LinkedList<PeerInfo> unchokablePeers)
     {
-        unchokable.clear();                                                   //changes
+        unchokable.clear();                                                  
         unchokable = unchokablePeers;
         unchokablePeerIds = PeerInfo.getPeerIds(unchokablePeers);
     }
@@ -33,25 +29,27 @@ public class OptimisticUnchoke implements Runnable {
         }
         if(!unchokable.isEmpty() && unchokable.size()>0)
         {
-            //Simple way of selecing unchokable neighbor
-            Collections.shuffle(unchokable);
-            int peerId = unchokable.getFirst().peerId;
+           
+//            Collections.shuffle(unchokable);
+//            int peerId = unchokable.getFirst().peerId;
+            int index=new Random().nextInt(unchokable.size());
+            int peerId=unchokable.get(index).peerId;
 
             logrecord.getLogRecord().changeOfOptimisticallyUnchokedNeighbors(peerId);
-           // logrecord.getLogRecord().changeOfOptimisticallyUnchokedNeighbors(peerId);
-            //sends unchoking message
+           
+            
             if(_peerSetupObj.connectionsList!=null){
             Iterator<Handler> it = _peerSetupObj.connectionsList.iterator();
 
 
             while(it.hasNext())
             {
-                //System.out.println("to send unchoke in unoptimistic unchoke thread");
+               
                 Handler newHandler = (Handler)it.next();
                 Collection<Integer> peersToUnchoke = new Vector<Integer>();
                 if(newHandler.remotePeerId.get() == peerId)
                 {
-                    //create a new collection and add this connection Handler to it. Then send it to peerSetUp to unchoking the peer
+                    
                     peersToUnchoke.add(peerId);
                 }
                 _peerSetupObj.unchokePeers(peersToUnchoke);
